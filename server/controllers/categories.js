23916c6f2cf2d97e3771bf9b8a2cfea01c7f3140
momo-dev1 <didy1234567@gmail.com>
2022-07-18 21:37:01 +0200

    skip & limit category

--- server/controllers/categories.js
+++ server/controllers/categories.js
@@ -8,8 +8,19 @@ const Category = require("../models/category")
     @access  Public
 */
 const getAllCategories = asyncHnadler(async (req, res) => {
-    const categories = await Category.find({})
-    res.status(200).json({ results: getAllCategories.length, data: categories })
+
+    const queryObject = {};
+
+    let result = Product.find(queryObject)
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    result = result.skip(skip).limit(limit)
+
+    const categories = await result
+    res.status(200).json({ num_Results: getAllCategories.length, page, data: categories })
 })
 
 /*
